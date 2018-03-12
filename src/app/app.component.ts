4de7d5d33f15afbebf2fbc64fda4fa407a309e04
minhvu80 <minh@shoemetro.com>
2018-03-11 17:45:38 -0700

    change me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Recipe } from './model/recipe';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  recipes: Recipe[];
+
+  constructor() {
+    this.recipes = [
+      new Recipe('Banana Bread', 'This is my favorite bread', null, null, null),
+      new Recipe('PadThai', 'They giftwrap it with eggs', null, null, null),
+      new Recipe('Pho', 'Pho here is so delicious', null, null, null),
+      new Recipe('Fried Rice', 'The rice is very fluffy', null, null, null),
+    ];
+  }
 }
