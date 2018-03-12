080fbc0016312df73ebf8f6a662cdedddc1564f2
minhvu80 <minh@shoemetro.com>
2018-03-11 18:08:15 -0700

    change priority of recipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,10 @@ export class AppComponent {
 
   constructor() {
     this.recipes = [
-      new Recipe('Banana Bread', 'This is my favorite bread', null, null, null),
       new Recipe('Pho', 'Pho here is so delicious', null, null, null),
       new Recipe('PadThai', 'They giftwrap it with eggs', null, null, null),
       new Recipe('Fried Rice', 'The rice is very fluffy', null, null, null),
+      new Recipe('Banana Bread', 'This is my favorite bread', null, null, null),
     ];
   }
 }
