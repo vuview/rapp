2904ebaa2eac9752a05d949c7e1577f6341117a2
vuview <adorkable81@gmail.com>
2018-03-12 20:23:08 -0700

    Add recipe add-on for action

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
 import { RecipeSummaryComponent } from './components/recipe-summary/recipe-summary.component';
+import { RecipeAddonComponent } from './components/recipe-addon/recipe-addon.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipeListComponent,
-    RecipeSummaryComponent
+    RecipeSummaryComponent,
+    RecipeAddonComponent
   ],
   imports: [
     BrowserModule
